904cf1435ee4530b406eb605dcba2c2e8c90879c
Likha Jacob <likha.jacob03@gmail.com>
2025-02-16 21:51:19 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -4,6 +4,12 @@ function calculateSalary(baseSalary, bonus, taxRate) {
     console.log (`Net Salary: $${netSalary.toFixed(2)}`)
     return netSalary
 };//Function to compute net salary
-
+//Test Data
 calculateSalary(5000, 500, 0.1); //Output: Net Salary: $ 5000.00
 calculateSalary(7000, 1000, 0.15); //Output: Net Salary: $6950.00s
+// Task 2 Product Price after Discount 
+let calculateDiscount = (price, discountRate) => price - (price * discountRate);
+
+// Test Data 
+console.log(`Final Price: $${calculateDiscount(100, 0.2).toFixed(2)}`);
+console.log(`Final Price: $${calculateDiscount(250, 0.15).toFixed(2)}`);
