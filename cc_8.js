385e68d9d6f057c41933f2ec5f88800778547e4d
Likha Jacob <likha.jacob03@gmail.com>
2025-02-16 22:38:43 -0500

    Task 4 - Car Rental Cost Calculation

--- cc_8.js
+++ cc_8.js
@@ -31,3 +31,29 @@ let calculateServiceFee = (amount, serviceType) => {
 
 calculateServiceFee(200, "Premium"); //Output: Service Fee: $30.00
 calculateServiceFee(500, "Standard"); //Output: Service Fee: $50.00
+// Task 4 Car Rental Cost Calculation 
+function calculateRentalCost(days, carType, insurance = false){
+    let dailyRate = 0;
+
+    // Daily rates 
+    if (carType === "Economy") {
+        dailyRate = 40; 
+    } else if (carType === "Standard") {
+        dailyRate = 60; 
+    } else if (carType === "luxury") {
+        dailyRate = 100;
+    }
+
+    // Calculation 
+    let totalCost = dailyRate * days;
+
+    // Insurance 
+    if (insurance) {
+        totalCost += 20 * days; 
+    }
+    return totalCost
+}
+
+// Test data 
+console.log(calculateRentalCost(3, "Economy", true));
+console.log(calculateRentalCost(5, "Luxury", false));
